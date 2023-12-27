cf5297c0c0406338e23b42fa5ed9f739ca550f02
chapman-blockchain <limth1312@naver.com>
2023-12-27 11:13:17 +0900

    Update .gitignore to exclude uploads directory

--- backend/src/board/images.js
+++ backend/src/board/images.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
       },
       Boards_id: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: true,
         references: {
           model: "Boards",
           key: "Boards_id",
